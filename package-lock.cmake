421e71d1dd140cb128cdc3760273c29133d7a92d
Marlon Beijer <marlon@eevul.org>
2025-10-29 23:52:01 +0100

    Include version in aminet readme

--- package-lock.cmake
+++ package-lock.cmake
@@ -8,12 +8,12 @@ CPMDeclarePackage(CPMLicenses.cmake
 )
 # Common CMake additions for Amiga
 CPMDeclarePackage(CMakeAmigaCommon
-  GIT_TAG 1.0.5
+  GIT_TAG 1.0.7
   GITHUB_REPOSITORY AmigaPorts/cmake-amiga-common-library
 )
 # Emu68 devicetree.resource
 CPMDeclarePackage(devicetree.resource
   GIT_TAG 54a2e8d068d1cedad00714d5ea45f5ac18f9b0b1
   GITHUB_REPOSITORY michalsc/devicetree.resource
-  EXCLUDE_FROM_ALL
+  EXCLUDE_FROM_ALL YES
 )
